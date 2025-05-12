9a99835810fc6b73599e28af50073fcf3cf3db49
ko-taku <kosumgul@naver.com>
2025-05-12 15:20:02 +0900

    complete

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,13 +1,29 @@
 const flights = require('../repository/flightList');
+const fs = require('fs');
 // 항공편 예약 데이터를 저장합니다.
 let booking = [];
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   // [GET] /book 요청을 수행합니다.
   // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    const { flight_uuid, phone } = req.query;
+
+    if (flight_uuid) {
+      const filtered = booking.filter(b => b.flight_uuid === flight_uuid);
+      return res.status(200).json(filtered);
+    }
+
+    if (phone) {
+      const found = booking.find(b => b.phone === phone);
+      if (found) {
+        return res.status(200).json(found);
+      }
+    }
+
+    return res.status(200).json(booking);
   },
 
   // [POST] /book 요청을 수행합니다.
@@ -17,13 +33,41 @@ module.exports = {
   // 참고 링크: https://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api#useful-post-responses
   create: (req, res) => {
     // TODO:
-    return res.status(201).json({});
+    const data = req.body;
+    //const uuid = uuidv4();
+    // console.log('유니크 아이디 : ', uuid);
+
+    // const bookingData = { uuid, ...data };
+    const bookingData = { ...data };
+
+    booking.push(bookingData);
+    // console.log('예약 정보 : ', bookingData);
+    return res.status(201).json(data.uuid);
   },
 
   // [DELETE] /book?phone={phone} 요청을 수행합니다.
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    const { phone } = req.query;
+
+    if (!phone) {
+      return res.status(400).json({ message: 'Phone number required' });
+    }
+
+    const originalLength = booking.length;
+    // phone이 일치하지 않는 예약만 남김
+    const remaining = booking.filter(b => b.phone !== phone);
+
+    if (remaining.length === originalLength) {
+      return res.status(404).json({ message: 'Booking not found' });
+    }
+
+    // 기존 배열 내용 갱신
+    booking.length = 0;
+    booking.push(...remaining);
+
+    return res.status(200).json(booking);
+
   },
 };
--- src/controller/flightController.js
+++ src/controller/flightController.js
@@ -1,17 +1,36 @@
 const flights = require('../repository/flightList');
+const fs = require('fs');
 
 module.exports = {
   // [GET] /flight
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: (req, res) => {
     // TODO:
-    return res.status(200).send('not implemented');
+    const { departure_times, arrival_times, destination, departure } = req.query;
+
+    const filtered = flights.filter(flight => {
+      return (
+        (!departure_times || flight.departure_times === departure_times) &&
+        (!arrival_times || flight.arrival_times === arrival_times) &&
+        (!destination || flight.destination === destination) &&
+        (!departure || flight.departure === departure)
+      );
+    });
+    return res.status(200).json(filtered);
   },
   // [GET] /flight/:id
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: (req, res) => {
     // TODO:
-    return res.status(200).json('not implemented');
+    const { id } = req.params;
+
+    const flight = flights.find(f => f.uuid === id);
+
+    if (!flight) {
+      return res.status(404).json({ message: 'Flight not found' });
+    }
+
+    return res.status(200).json([flight]);
   },
 
   // [PUT] /flight/:id 요청을 수행합니다.
@@ -20,6 +39,23 @@ module.exports = {
     let data;
     // TODO:
 
+    const { id } = req.params;
+    updateData = req.body;
+
+    const index = flights.findIndex(f => f.uuid === id);
+
+    data = flights[index];
+
+    if (index === -1) {
+      return res.status(404).json({ message: 'Flight not found' });
+    }
+
+    data = {
+      ...data,
+      ...updateData,
+    };
+
     return res.status(200).json(data);
+
   },
 };
